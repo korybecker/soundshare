fe9124fce251b80a0a1d615fba8c852042fca2ef
Kory <korymbecker@gmail.com>
2022-04-09 20:31:29 -0400

    c

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,7 +44,7 @@ const Login = () => {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
+          <h2 className="text-center mb-4">LOG IN</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
